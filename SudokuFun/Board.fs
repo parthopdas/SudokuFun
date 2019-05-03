fa3d15ce801cfa5decc18ae9c8007b0634c6d5e5
Partho P. Das <parthopdas@live.com>
2019-05-03 23:20:35 +1000

    Can import puzzles generated online.

--- SudokuFun/Board.fs
+++ SudokuFun/Board.fs
@@ -3,12 +3,27 @@
 type Board =
     | Board of Set<int>[,] 
 
-let initBoard (): Board =
+(*
+  To generate new puzzls:
+  - Go to https://qqwing.com/generate.html
+  - Select output format as single line
+ *)
+let private getNewCandidates candidates leaveOut =
+    candidates
+    |> List.filter (fun x -> x <> leaveOut)
+
+let initBoard str: Board =
+    if (String.length str <> 81) then
+        failwith "Input must be a string of 81 characters"
+
     let b = Array2D.create 9 9 Set.empty
 
     seq { for i in 0 .. 8 do
             for j in 0 .. 8 do
                 yield (i, j) }
-    |> Seq.iter (fun (i, j) -> b.[i,j] <- Set.ofSeq [1..9])
+    |> Seq.iter (
+        fun (i, j) -> 
+            let s = Set.ofSeq [1..9] |> Set.remove (int str.[i * 9 + j] - int '0')
+            b.[i,j] <- Set.ofSeq s)
     
     b |> Board
--- SudokuFun/Program.fs
+++ SudokuFun/Program.fs
@@ -20,6 +20,6 @@
 
 [<EntryPoint>]
 let main _ =
-    let b = SudokuFun.initBoard ()
+    let b = SudokuFun.initBoard ".47..1.5.9....5.......7....7.5.92.4.4..56..1781..4..2.1....4.....27......6..29..."
     printfn "%A" b
     0 // return an integer exit code
