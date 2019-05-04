ed50ef424e074c5fef63b147b2c3d7c477e3933e
Partho P. Das <parthopdas@live.com>
2019-05-04 16:04:21 +1000

    Crosshatching strategy implemented. Can solve simple ones.

--- SudokuFun/Board.fs
+++ SudokuFun/Board.fs
@@ -5,17 +5,24 @@ open System.Text
 type Board =
     | Board of Set<int>[,] 
 
+let defaultCellCandidates = Set.ofSeq [ 1..9 ]
+
 let cellIndices =
     seq { for i in 0 .. 8 do
             for j in 0 .. 8 do
                 yield (i, j) }
     |> List.ofSeq
 
+let nonetIndicesMap =
+    cellIndices
+    |> Seq.groupBy (fun (i, j) -> (i / 3, j / 3))
+    |> Map.ofSeq
+
 ///
 /// To generate new puzzle:
 /// 1. Go to https://qqwing.com/generate.html
 /// 2. Select output format as single line
-let initBoard str: Board =
+let initBoard str =
     if (String.length str <> 81) then
         failwith "Input must be a string of 81 characters"
 
@@ -25,33 +32,73 @@ let initBoard str: Board =
     |> Seq.iter (
         fun (i, j) -> 
             let c = (int str.[i * 9 + j] - int '0')
-            let s = if c >= 1 && c <= 9 then [ c ] else [1..9]
-            b.[i,j] <- Set.ofSeq s)
+            let s = if c >= 1 && c <= 9 then Set.singleton c else defaultCellCandidates
+            b.[i,j] <- s)
     
     b |> Board
 
+let getSetOfFilledCells : seq<Set<int>> -> Set<int> =
+    Seq.filter (fun x -> x.Count = 1)
+    >> Seq.map Seq.exactlyOne
+    >> Set.ofSeq
+
+let getNonetOccupancy (Board b) i j =
+    nonetIndicesMap
+    |> Map.find (i, j)
+    |> Seq.map (fun (i, j) -> b.[i, j])
+    |> getSetOfFilledCells
+
+let getRowOccupancy (Board b) i =
+    b.[i, *]
+    |> getSetOfFilledCells
+
+let getColumnOccupancy (Board b) j =
+    b.[*, j]
+    |> getSetOfFilledCells
+
+let crossHatchCell (Board b) i j =
+    if b.[i,j].Count = 1 then
+        ()
+    else
+        let nSet = getNonetOccupancy (Board b) (i / 3) (j / 3)
+        let rSet = getRowOccupancy (Board b) i
+        let cSet = getColumnOccupancy (Board b) j   
+        let candidates = Set.difference defaultCellCandidates (Set.unionMany [ nSet; rSet; cSet ])
+        b.[i,j] <- candidates
+        
+let numberOfHints (Board b) =
+    cellIndices
+    |> Seq.filter (fun (i, j) -> b.[i,j].Count = 1)
+    |> Seq.length
+
+let crossHatch (Board b) =
+    cellIndices
+    |> Seq.iter (fun (i, j) -> crossHatchCell (Board b) i j)
+    numberOfHints (Board b)
+
+let isSolved b = numberOfHints b = 81
+
 let printBoard (Board b) =
     let boardStr = 
-        [|
-          "╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗"
-          "║   |   |   ║   |   |   ║   |   |   ║"
-          "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢"
-          "║   |   |   ║   |   |   ║   |   |   ║"
-          "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢"
-          "║   |   |   ║   |   |   ║   |   |   ║"
-          "╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣"
-          "║   |   |   ║   |   |   ║   |   |   ║"
-          "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢"
-          "║   |   |   ║   |   |   ║   |   |   ║"
-          "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢"
-          "║   |   |   ║   |   |   ║   |   |   ║"
-          "╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣"
-          "║   |   |   ║   |   |   ║   |   |   ║"
-          "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢"
-          "║   |   |   ║   |   |   ║   |   |   ║"
-          "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢"
-          "║   |   |   ║   |   |   ║   |   |   ║"
-          "╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝"|]
+        [| "╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗"
+           "║   │   │   ║   │   │   ║   │   │   ║"
+           "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢"
+           "║   │   │   ║   │   │   ║   │   │   ║"
+           "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢"
+           "║   │   │   ║   │   │   ║   │   │   ║"
+           "╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣"
+           "║   │   │   ║   │   │   ║   │   │   ║"
+           "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢"
+           "║   │   │   ║   │   │   ║   │   │   ║"
+           "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢"
+           "║   │   │   ║   │   │   ║   │   │   ║"
+           "╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣"
+           "║   │   │   ║   │   │   ║   │   │   ║"
+           "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢"
+           "║   │   │   ║   │   │   ║   │   │   ║"
+           "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢"
+           "║   │   │   ║   │   │   ║   │   │   ║"
+           "╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝" |]
         |> Array.map String.toCharArray
 
     cellIndices
@@ -64,3 +111,14 @@ let printBoard (Board b) =
     |> Seq.map String.fromCharArray
     |> Seq.fold StringBuilder.appendLine (StringBuilder())
     |> printfn "%A"
+
+let solve b =
+    let rec solveImpl h0 p b =
+        if (isSolved b) then
+            p
+        else
+            let h1 = crossHatch b
+            if h0 = h1 then failwithf "Unable to solve any futher after %d passes. Apply other strategy(s)." (p + 1)
+            printBoard b
+            solveImpl h1 (p + 1) b
+    solveImpl (numberOfHints b) 0 b
--- SudokuFun/Program.fs
+++ SudokuFun/Program.fs
@@ -22,5 +22,8 @@
 let main _ =
     let b = SudokuFun.initBoard ".47..1.5.9....5.......7....7.5.92.4.4..56..1781..4..2.1....4.....27......6..29..."
     SudokuFun.printBoard b
-    printfn "%A" b
-    0 // return an integer exit code
+    let passes = SudokuFun.solve b
+    printfn "Number of passses = %d" passes    
+    SudokuFun.printBoard b
+
+    0
